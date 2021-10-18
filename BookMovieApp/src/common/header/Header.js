3ca64db6ccd40c6500058339f0d8340fdfc4a15b
Nithin1296 <nithin.nik44@gmail.com>
2021-10-18 23:43:36 +0530

    "Minor Changes"

--- BookMovieApp/src/common/header/Header.js
+++ BookMovieApp/src/common/header/Header.js
@@ -1,12 +1,28 @@
-import React, {Fragment,div, useEffect, useState} from 'react';
+import React, { Component, useState } from 'react';
 import './Header.css';
-import { ReactComponent as logo } from './assets/logoComponent.js';
+import logo from '../../assets/logo.svg';
+import Button from '@material-ui/core/Button';
 
-function Header() {
+function Header(props) {
+    
+  const [counter,setCounter] = useState(0);
+  const [username,setName] = useState("");
+
     return(
+
+        <div>
         <div className='logo'>
-            <logo/>
+            <img src = {logo} className= 'logo' alt='logo'/>
+        </div>
+        <div className='loginButton'>
+            <Button variant="contained" color="default" >Login</Button>
+        </div>
+        <div className='loginButton'>
+            <Button variant="contained" color="default" >Logout</Button>
+        </div>
         </div>
     );
 }
+
+
 export default Header;
---
+++ BookMovieApp/src/screens/home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component, useState } from 'react';
+import './Home.css';
+
+function Home() {
+
+    return(
+
+        <div>
+        <div className='home'>
+        <h4>Upcoming Movies</h4>
+        </div>
+        </div>
+    );
+}
+
+
+export default Home;
